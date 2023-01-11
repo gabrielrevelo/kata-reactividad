7cb68bb99be8695d812510b6b84ce91c8e4c6084
GaRev-Dev <revelo.gabriel.38@gmail.com>
2023-01-10 20:58:20 -0500

    Return a Mono<User> containing User.SAUL when an error occurs in the input Mono, else do not change the input Mono

--- src/main/java/io/pivotal/literx/Part07Errors.java
+++ src/main/java/io/pivotal/literx/Part07Errors.java
@@ -33,7 +33,7 @@ public class Part07Errors {
 
 	// TODO Return a Mono<User> containing User.SAUL when an error occurs in the input Mono, else do not change the input Mono.
 	Mono<User> betterCallSaulForBogusMono(Mono<User> mono) {
-		return null;
+		return mono.onErrorReturn(User.SAUL);
 	}
 
 //========================================================================================
